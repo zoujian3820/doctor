73c0dee6ece81e44a2feddb9259b2933dfdb6043
mrzou <382066178@qq.com>
2020-05-15 18:01:58 +0800

    opt(): new page

---
+++ pages/online_edit/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="page">home</div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  head() {
+    return {
+      title: '在线开方'
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss"></style>
